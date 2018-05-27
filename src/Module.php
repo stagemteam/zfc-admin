a134f15cb97f55fd628e1e3bf15bc02d2c4bea88
popovsergiy <popow.sergiy@gmail.com>
2018-05-27 12:04:52 +0300

    Empty service_manager config has been fixed

--- src/Module.php
+++ src/Module.php
@@ -7,8 +7,8 @@ class Module
     public function getConfig()
     {
         $config = include __DIR__ . '/../config/module.config.php';
-        $config['service_manager'] = $config['dependencies'];
-        unset($config['dependencies']);
+        //$config['service_manager'] = $config['dependencies'];
+        //unset($config['dependencies']);
 
         return $config;
     }
