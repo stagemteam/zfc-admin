c1ae7cd40b231a8eee766cdb62ebef6b92f98e4b
popovsergiy <popow.sergiy@gmail.com>
2018-05-29 09:15:04 +0300

    Styles have been changed

--- config/module.config.php
+++ config/module.config.php
@@ -23,15 +23,15 @@ return [
         'not_found_template' => 'error/404',
         'exception_template' => 'error/index',
         'template_map' => [
-            'layout/layout' => __DIR__ . '/../view/layout/default.phtml',
-            'layout/ajax' => __DIR__ . '/../view/layout/ajax.phtml',
+            'layout::admin' => __DIR__ . '/../view/layout/admin.phtml',
+            #'layout/ajax' => __DIR__ . '/../view/layout/ajax.phtml',
             'widget/breadcrumb' => __DIR__ . '/../view/widget/breadcrumb.phtml',
             'widget/flashMessages' => __DIR__ . '/../view/widget/flashMessages.phtml',
             'error/404' => __DIR__ . '/../view/error/404.phtml',
             'error/index' => __DIR__ . '/../view/error/index.phtml',
         ],
-        'template_path_stack' => [
-            __NAMESPACE__ => __DIR__ . '/../view',
+        'prefix_template_path_stack' => [
+            'admin::' => __DIR__ . '/../view',
         ],
     ],
 
--- src/Action/Admin/DashboardAction.php
+++ src/Action/Admin/DashboardAction.php
@@ -17,16 +17,16 @@ namespace Stagem\ZfcAdmin\Action\Admin;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
-use Psr\Http\Server\MiddlewareInterface;
-
+#use Psr\Http\Server\RequestHandlerInterface;
+use Interop\Http\ServerMiddleware\MiddlewareInterface;
+use Interop\Http\Server\RequestHandlerInterface;
 use Zend\View\Model\ViewModel;
 
 class DashboardAction implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $viewModel = new ViewModel(['message'=> '<h1>Hello, Ukraine!</h1>']);
+        $viewModel = new ViewModel(['message'=> '<h1>Hi there!</h1>']);
 
         return $handler->handle($request->withAttribute(ViewModel::class, $viewModel));
     }
